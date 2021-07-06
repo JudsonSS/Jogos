475e46f989a77d5bf9068a9802ddc4e54cd947f5
Judson Santiago <judson@ufersa.edu.br>
2021-07-06 01:21:41 -0300

    Slides da Aula 00 atualizados

--- Labs/Lab01/MessageBox/MessageBox/MsgBox.cpp
+++ Labs/Lab01/MessageBox/MessageBox/MsgBox.cpp
@@ -19,30 +19,30 @@ using std::stringstream;
 
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
-	int resp = MessageBox(NULL, 
-					      "Vamos calcular o tamanho da tela?",
-						  "Mensagem", 
-						  MB_YESNO | MB_DEFBUTTON2 | MB_ICONQUESTION) ;
+    int resp = MessageBox(NULL, 
+                          "Vamos calcular o tamanho da tela?",
+                          "Mensagem", 
+                          MB_YESNO | MB_DEFBUTTON2 | MB_ICONQUESTION) ;
 
-	if (resp == IDYES)
-	{
-		int cxScreen = GetSystemMetrics(SM_CXSCREEN);
-		int cyScreen = GetSystemMetrics(SM_CYSCREEN);
+    if (resp == IDYES)
+    {
+        int cxScreen = GetSystemMetrics(SM_CXSCREEN);
+        int cyScreen = GetSystemMetrics(SM_CYSCREEN);
 
-		stringstream text;
-		text << "Resolução: " << cxScreen << " x " << cyScreen;
-		
-		MessageBox(NULL, 
-		           text.str().c_str(), 
-		           "Tamanho da Tela", 
-				   MB_OK | MB_ICONINFORMATION);
-	} else {
+        stringstream text;
+        text << "Resolução: " << cxScreen << " x " << cyScreen;
+        
+        MessageBox(NULL, 
+                   text.str().c_str(), 
+                   "Tamanho da Tela", 
+                   MB_OK | MB_ICONINFORMATION);
+    } else {
 
-		MessageBox(NULL, 
-		           "Um jogo deve conhecer o tamanho da tela!", 
-		           "Mensagem", 
-				   MB_OK | MB_ICONWARNING);
-	}
+        MessageBox(NULL, 
+                   "Um jogo deve conhecer o tamanho da tela!", 
+                   "Mensagem", 
+                   MB_OK | MB_ICONWARNING);
+    }
 
     return 0 ;
 }
