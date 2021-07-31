03db4b6356bb6cb5d96f3d5bd904d1979622ded1
Judson Santiago <judson@ufersa.edu.br>
2021-07-31 00:45:01 -0300

    Adicionado Lab04

--- Labs/Lab04/GameLoop/GameLoop/GameLoop.cpp
+++ Labs/Lab04/GameLoop/GameLoop/GameLoop.cpp
@@ -1,17 +1,17 @@
 /**********************************************************************************
 // GameLoop
 // 
-// Criação:		19 Ago 2011
-// Atualização:	09 Mai 2019
-// Compilador:	Visual C++ 2019
+// Criação:     19 Ago 2011
+// Atualização: 31 Jul 2021
+// Compilador:  Visual C++ 2019
 //
-// Descrição:	Esqueleto de um laço de tempo real para criar um jogo.
+// Descrição: Esqueleto de um laço de tempo real para criar um jogo.
 //
 **********************************************************************************/
 
-#include <windows.h>	// inclui funções do windows
-#include <windowsx.h>	// inclui funções extras do windows
-#include "Resources.h"  // definições dos recursos utilizados
+#include <windows.h>        // inclui funções do windows
+#include <windowsx.h>       // inclui funções extras do windows
+#include "Resources.h"      // definições dos recursos utilizados
 
 // protótipo do procedimento da janela
 LRESULT CALLBACK WinProc (HWND, UINT, WPARAM, LPARAM);
@@ -19,12 +19,12 @@ LRESULT CALLBACK WinProc (HWND, UINT, WPARAM, LPARAM);
 // -----------------------------------------------------------------------
 // constantes, variáveis e funções globais
 
-int windowWidth  = 960;		// largura da janela
-int windowHeight = 540;		// altura da janela
-bool fullScreen = false;	// tela cheia ou modo janela
+int windowWidth  = 960;     // largura da janela
+int windowHeight = 540;     // altura da janela
+bool fullScreen = false;    // tela cheia ou modo janela
 
-bool vkKeys[256] = { 0 };	// estado das teclas do teclado
-HWND hwnd;					// identificador da janela
+bool vkKeys[256] = { 0 };   // estado das teclas do teclado
+HWND hwnd;                  // identificador da janela
 
 // -----------------------------------------------------------------------
 // funções do jogo
@@ -37,9 +37,9 @@ void GameInit()
 // atualiza a lógica do jogo
 void GameUpdate()
 {
-	// sai do jogo com o pressionamento da tecla ESC
-	if (vkKeys[VK_ESCAPE])
-		PostMessage(hwnd, WM_DESTROY, 0, 0);
+    // sai com o pressionamento da tecla ESC
+    if (vkKeys[VK_ESCAPE])
+        PostMessage(hwnd, WM_DESTROY, 0, 0);
 }
 
 // desenha o próximo quadro do jogo
@@ -60,7 +60,7 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     MSG			msg;
     WNDCLASS	wndclass; 
 
-	// definindo uma classe de janela chamada "GameWindow" 
+    // definindo uma classe de janela chamada "GameWindow" 
     wndclass.style         = CS_HREDRAW | CS_VREDRAW;
     wndclass.lpfnWndProc   = WinProc;
     wndclass.cbClsExtra    = 0;
@@ -72,91 +72,90 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     wndclass.lpszMenuName  = NULL;
     wndclass.lpszClassName = "GameWindow";
 
-	// registrando "GameWindow"
+    // registrando "GameWindow"
     if (!RegisterClass (&wndclass))
     {
          MessageBox (NULL, "Erro na criação da janela!", "Game", MB_ICONERROR);
          return 0 ;
     }
 
-	int windowStyle = WS_OVERLAPPED | WS_SYSMENU;
+    int windowStyle = WS_OVERLAPPED | WS_SYSMENU;
 
-	// tela cheia ou janela
-	if (fullScreen)
-	{
-		windowWidth  = GetSystemMetrics(SM_CXSCREEN);
-		windowHeight = GetSystemMetrics(SM_CYSCREEN);
-		windowStyle  = WS_POPUP;
-	}
+    // tela cheia ou janela
+    if (fullScreen)
+    {
+        windowWidth  = GetSystemMetrics(SM_CXSCREEN);
+        windowHeight = GetSystemMetrics(SM_CYSCREEN);
+        windowStyle  = WS_POPUP;
+    }
 
-	// criando uma janela baseada na classe "GameWindow"
-	// é necessário usar CreateWindowEx para usar AdjustWindowRectEx
+    // criando uma janela baseada na classe "GameWindow"
+    // é necessário usar CreateWindowEx para usar AdjustWindowRectEx
     hwnd = CreateWindowEx(
-			NULL,							// estilos extras
-			"GameWindow",					// nome da "window class"
-			"Game",							// título da janela
-			windowStyle,					// estilo da janela
-			CW_USEDEFAULT,					// posição x inicial
-			CW_USEDEFAULT,					// posição y inicial
-			windowWidth,					// largura da janela
-			windowHeight,					// altura da janela
-			NULL,							// identificador do objeto pai
-			NULL,							// identificador do menu
-			hInstance,						// identificador da aplicação
-			NULL);							// parâmetros de criação
+            NULL,                           // estilos extras
+            "GameWindow",                   // nome da "window class"
+            "Game",                         // título da janela
+            windowStyle,                    // estilo da janela
+            CW_USEDEFAULT,                  // posição x inicial
+            CW_USEDEFAULT,                  // posição y inicial
+            windowWidth,                    // largura da janela
+            windowHeight,                   // altura da janela
+            NULL,                           // identificador do objeto pai
+            NULL,                           // identificador do menu
+            hInstance,                      // identificador da aplicação
+            NULL);                          // parâmetros de criação
 
-	if (!fullScreen)
-	{
-		// retângulo com o tamanho da área cliente desejada
-		RECT winRect = {0, 0, windowWidth, windowHeight};
+    if (!fullScreen)
+    {
+        // retângulo com o tamanho da área cliente desejada
+        RECT winRect = {0, 0, windowWidth, windowHeight};
 
-		// ajuste do tamanho da janela
-		AdjustWindowRectEx(&winRect,
-						   GetWindowStyle(hwnd),
-						   GetMenu(hwnd) != NULL,
-						   GetWindowExStyle(hwnd));
+        // ajuste do tamanho da janela
+        AdjustWindowRectEx(&winRect,
+                           GetWindowStyle(hwnd),
+                           GetMenu(hwnd) != NULL,
+                           GetWindowExStyle(hwnd));
 
-		// atualiza posição da janela
-		int windowPosX = (GetSystemMetrics(SM_CXSCREEN)/2) - ((winRect.right - winRect.left)/2);
-		int windowPosY = (GetSystemMetrics(SM_CYSCREEN)/2) - ((winRect.bottom - winRect.top)/2);
+        // atualiza posição da janela
+        int windowPosX = (GetSystemMetrics(SM_CXSCREEN)/2) - ((winRect.right - winRect.left)/2);
+        int windowPosY = (GetSystemMetrics(SM_CYSCREEN)/2) - ((winRect.bottom - winRect.top)/2);
 
-		// redimensiona janela com uma chamada a MoveWindow
-		MoveWindow(
-			hwnd,							// identificador da janela
-			windowPosX,						// posição x
-			windowPosY,						// posição y
-			winRect.right - winRect.left,	// largura
-			winRect.bottom - winRect.top,	// altura
-			TRUE);							// repintar
-	}
+        // redimensiona janela com uma chamada a MoveWindow
+        MoveWindow(
+            hwnd,                           // identificador da janela
+            windowPosX,                     // posição x
+            windowPosY,                     // posição y
+            winRect.right - winRect.left,   // largura
+            winRect.bottom - winRect.top,   // altura
+            TRUE);                          // repintar
+    }
 
-	// mostra e atualiza a janela
+    // mostra e atualiza a janela
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-	GameInit();
+    GameInit();
      
-	// loop principal do jogo
+    // loop principal do jogo
     do
     {
-		if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		} 
-		else 
-		{
-			GameUpdate();
-			GameDraw();
-		}
-	} 
-	while (msg.message != WM_QUIT);
+        if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        } 
+        else 
+        {
+            GameUpdate();
+            GameDraw();
+        }
+    } 
+    while (msg.message != WM_QUIT);
 
-	GameFinalize();
+    GameFinalize();
 
-
-	// fim do programa
-    return msg.wParam;
+    // fim do programa
+    return int(msg.wParam);
 }
 
 // -----------------------------------------------------------------------
@@ -166,19 +165,19 @@ LRESULT CALLBACK WinProc (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 { 
     switch (message)
     {
-		// processa teclas pressionadas
-	case WM_KEYDOWN:
-		vkKeys[wParam] = true;
-		return 0;
+        // processa teclas pressionadas
+    case WM_KEYDOWN:
+        vkKeys[wParam] = true;
+        return 0;
 
-		// processa teclas liberadas
-	case WM_KEYUP:
-		vkKeys[wParam] = false;
-		return 0;
-		
-		// janela foi fechada
+        // processa teclas liberadas
+    case WM_KEYUP:
+        vkKeys[wParam] = false;
+        return 0;
+        
+        // janela foi fechada
     case WM_DESTROY:
-		PostQuitMessage (0);
+        PostQuitMessage (0);
         return 0;
     }
 
